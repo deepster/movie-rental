c8ebe48c961705b20027f89c3f3618c4bd848416
deepster <craciunescu.denis12@gmail.com>
2019-05-26 00:18:35 +0300

    Add routing module

--- web/src/main/webapp/src/app/app.module.ts
+++ web/src/main/webapp/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
