35acfd499a2768605e1e624cbc21efe6589509aa
deepster <craciunescu.denis12@gmail.com>
2019-05-26 00:27:31 +0300

    Display mock movie list

--- web/src/main/webapp/src/app/movie-list/movie-list.component.ts
+++ web/src/main/webapp/src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from "../shared/Movie";
+
+export const movies: Movie[] =
+  [
+    {id : 1 ,title : 'Denis', genre : 'Action', rating : 10, year : 1998},
+    {id : 2 ,title : 'Denis', genre : 'Action', rating : 10, year : 1998},
+    {id : 3 ,title : 'Denis', genre : 'Action', rating : 10, year : 1998},
+    {id : 4 ,title : 'Denis', genre : 'Action', rating : 10, year : 1998},
+  ];
 
 @Component({
   selector: 'app-movie-list',
@@ -7,11 +16,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
+  moviesList = movies;
+
   movieListURL = "http://localhost:8080/api/movies";
 
-  movies = [];
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
